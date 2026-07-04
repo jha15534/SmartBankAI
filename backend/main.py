4444d4a1cea397f610f94e6e8430fa591d4e4ef8
Shruti Jha <133661363+jha15534@users.noreply.github.com>
2026-07-04 14:07:49 +0530

    Fix CORS for Vercel frontend

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173"
+        "https://smart-bank-ai-taupe.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
